e4add9e430a3d7999591fe17fb72342731b7d329
C. S <schady@posteo.de>
2016-10-02 00:02:26 +0200

    aa

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+    <meta http-equiv='pragma' content='no-cache'>
+    <meta http-equiv="Content-Language" content="de" />
+    <meta name="keywords" content="Vermietungen Gewerblich, Gewerbliche Vermietungen, Simon Vermietungen, SImon Gewerbliche Vermietungen" />
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  </head>
+  <body>
+    <center>
+    <div id="header">
+      <div id="logo">
+        <img src="img/simon.jpg">
+      </div>
+      <nav>
+        <a href="">HOME</a>
+        <a href="">PROFIL</a>
+        <a href="">LEISTUNGEN</a>
+        <a href="">KONTAKT</a>
+        <a href="">PLAN</a>
+        <a href="">IMPRESSUM</a>
+      </nav>
+    </div>
+    <div id="content">
+      <div style="color:#ff5808; text-align: left;font-size: 43px; padding-left: 170px;padding-top: 160px;">Gewerbliche<br/>Vermietungen:</div>
+      <ul style="color: white; font-size: 20px; text-align: left; padding-left: 170px; list-style:none;">
+        <li>Business Adressen</li>
+        <li>Büroräume</li>
+        <li>Lagerräume</li>
+        <li>Freiflächen außen</li>
+      </ul>
+    </div>
+    <div id="sub">
+      <div id="subpage">
+        <p style="color: #ff5808; font-size: 24px; font-weight: bold;">Unternehmensstandort Berlin</p>
+        <p style="font-size: 17px; color:#636363; width: 400px;">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div id="subsubpage">
+        <img src="img/home-tower.jpg">
+      </div>
+  </div>
+  </center>
+  </body>
+</html>
