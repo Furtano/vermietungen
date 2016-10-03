5b671b1c90b812d56970ccae689e0285f89aae7d
C. S <schady@posteo.de>
2016-10-03 12:54:19 +0200

    aa

--- leistungen.php
+++ leistungen.php
@@ -9,10 +9,10 @@
     </div>
     <div id="leistungencontent">
     </div>
-    <div id="sub">
+    <div id="sub" style="line-height: 23px;">
       <div id="subpage-leistungen">
         <p style="color: #ff5808; font-size: 21px; font-weight: bolder;">Business-Adressen (Briefkästen)</p>
-        <p style="font-size: 19px; color:#636363; width: 330px; margin-top: 0px;">
+        <p style="font-size: 19px; color:#636363; width: 330px; margin-top: 0px;margin-bottom: 0px;">
           Die SIMON GMBH bietet Ihnen die
           Möglichkeit am Standort Berlin mit
           einer virtuellen Adresse vertreten zu
@@ -20,9 +20,9 @@
           Bereits ab 25,- EUR/Monat zzgl. MwSt.
           erhalten Sie eine Firmenadresse auf
           unserem bekannten Gewerbehof mit
-          Briefkasten.
-          Optionen:
-          <ul style="font-size: 19px;color:#636363; ">
+          <strong>Briefkasten.</strong>
+          <p style="color: #ff5808; font-size: 19px; font-weight: bolder;margin: 0px;">Optionen:</p>
+          <ul style="font-size: 19px;color:#636363; margin-top: 15px; ">
             <li>Postweiterleitung täglich oder</li>
             <li>wöchentlich (Selbstabholung<br/>täglich 24 Std.)
             <li>Digitales Fax mit Weiterleitung an</li>
@@ -54,7 +54,7 @@
           monatlich, plus Nebenkosten und
           Mehrwertsteuer. Zuzüglich wird eine
           Kaution in Höhe von 3 Monatsmieten
-          bei Vertagsabschluß fällig.
+          bei Vertagsabschluß fällig.<br/><br/>
           Unser Gewerbehof ist bekannt für ein
           gutes Klima unter den Mietern. Zur Zeit
           besteht die Mieterschaft aus Hand-
@@ -67,7 +67,7 @@
 
         <p style="color: #ff5808; font-size: 21px; font-weight: bolder; margin-top: 0px;margin-bottom: 0px;padding-left: 30px;">Lagerräume, Freiflächen außen</p>
 
-        <p style="font-size: 19px; color:#636363; border-left: 1px grey solid; height: 385px;padding-left: 30px;margin-top: 15px;">
+        <p style="font-size: 19px; color:#636363; border-left: 1px grey solid; height: 365px;padding-left: 30px;margin-top: 15px;">
           Unsere Lagerräume sind wahlweise be-
           heizt oder unbeheizt. Die einzelnen
           Hauseingänge sind mit Auffahrten ausge-
@@ -79,8 +79,10 @@
           Lagerungen bieten wir Freiflächen in unter-
           schiedlichen Größen.
           <br/><br/>
-          Unser gesamtes Gelände wird von einem
-          Wachschutzunternehmen gesichert.
+          <span style="color: #ff5808;">
+            Unser gesamtes Gelände wird von einem
+            Wachschutzunternehmen gesichert.
+          </span>
       </p>
       </div>
   </div>
